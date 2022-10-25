06107811332974e11eefe7e090e665bb316b4ace
Kushal-Chandar <kushalchandariit@gmail.com>
2022-10-25 17:55:38 +0530

    feat: added support for linux

--- options.cmake
+++ options.cmake
@@ -4,11 +4,11 @@ include_guard()
 #   Uncomment to set options or set options in cmake-gui.
 # ----------------------------------------------------------------------------
 # set(BUILD_SHARED_LIBS ON)
-set(UPDATE_VCPKG ON)
-set(WARNINGS_AS_ERRORS ON)
+set(INSTALL_OR_UPDATE_VCPKG ON)
+# set(WARNINGS_AS_ERRORS ON)
 # set(CODE_COVERAGE ON) # enable in github actions
 set(BUILD_TESTING ON)
-set(ENABLE_DOXYGEN ON)
+# set(ENABLE_DOXYGEN ON)
 
 set(ENABLE_CCACHE ON)
 # set(ENABLE_IPO ON)
