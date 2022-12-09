a6331c16e8639a65bb14c0f80edc6ed26662ac83
Kushal-Chandar <kushalchandariit@gmail.com>
2022-12-09 23:35:09 +0530

    feat(configure_file): converted into interface target

--- src/impl/main.cc
+++ src/impl/main.cc
@@ -3,14 +3,14 @@
 #include <string_view>
 
 #include "cmake_init_lib.h"
-#include "configuration/config.h"
+#include "config.h"
 
 auto main() -> int {
   cmake_init::CmakeInit cmake_init(
       std::string_view("Creating cmake_init object"));
   cmake_init.Greet();
-  fmt::print("{}\n{}\n{}\n", cmake_init::configuration::git_sha,
-             cmake_init::configuration::project_name,
-             cmake_init::configuration::project_version);
+  fmt::print("{}\n{}\n{}\n", cmake_init::config::git_sha,
+             cmake_init::config::project_name,
+             cmake_init::config::project_version);
   return 0;
 }
