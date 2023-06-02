6ea69cc5b827c3c3abcc8f8ec0efacc95e109329
Kushal-Chandar <kushalchandariit@gmail.com>
2023-06-02 15:44:44 +0530

    fix: cli_test has merged back into main exectable

--- tests/cli_test.cc
+++ tests/cli_test.cc
@@ -12,12 +12,12 @@ TEST(CLITests, Parse) {
   // When --help command line arguments is given
   argv.emplace_back("--help");
   ASSERT_NE(cmake_init::CLI::Parse(2, argv.data()), std::nullopt);
-  argv.clear();
+  argv.pop_back();
 
   // When -e command line arguments is given
   argv.emplace_back("-e");
   ASSERT_NE(cmake_init::CLI::Parse(2, argv.data()), std::nullopt);
-  argv.clear();
+  argv.pop_back();
 
   // When both command line arguments are given (if --help is given help should
   // print first)
